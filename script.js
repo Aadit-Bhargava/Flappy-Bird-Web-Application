758077ce2942b29940f60f694e54953b4b668998
Aadit-Bhargava <bhargavaaadit@gmail.com>
2023-09-07 14:26:38 +0530

    Model 1 Completed

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const ground = document.querySelector('.ground')
 // Variables used to "affect" the elements in the game.
 let birdLeft = 220
 let birdBottom = 100
+let gap = 430
 let gravity = 2
+let isGameOver = false;
 
 /**
  * Method to begin the game with default positioning of the bird.
@@ -19,7 +21,7 @@ function startGame()
 }
 
 // Used to call the startGame method at a regular interval of 20 ms.
-let timeId = setInterval(startGame, 20)
+let gameTimerId = setInterval(startGame, 20)
 
 /**
  * Method to call the "jump" method if the space-bar key event is pressed.
@@ -54,41 +56,70 @@ function generateObstacle()
     // Generating an obstacle at a random height from the ground.
     let randomHeight = Math.random() * 60
 
-    let obstacleBotoom = randomHeight
+    let obstacleBottom = randomHeight
 
-    // Creates a div element called "obstacle".
-    const obstacle = document.createElement('div')
+    // Creates a div element called "bottomObstacle".
+    const bottomObstacle = document.createElement('div')
 
-    // Giving the created div variable obstacle a class name of "obstacle".
-    obstacle.classList.add('obstacle')
+    // Creates a div element called "topObstacle".
+    const topObstacle = document.createElement('div')
 
-    obstacle.style.left = obstacleLeft + 'px'
-    obstacle.style.bottom = obstacleBotoom + 'px'
+    // Giving the created div variable obstacle a class name of "bottomObstacle".
+    if(!isGameOver) {
+        bottomObstacle.classList.add('bottomObstacle')
+        topObstacle.classList.add('topObstacle')
+    }
+
+    bottomObstacle.style.left = obstacleLeft + 'px'
+    topObstacle.style.left = obstacleLeft + 'px'
+    bottomObstacle.style.bottom = obstacleBottom + 'px'
+    topObstacle.style.bottom = obstacleBottom + gap + 'px'
 
     // We are appending the newly created div to our parent div.
-    gameDisplay.appendChild(obstacle)
+    gameDisplay.appendChild(bottomObstacle)
+
+    // We are appending the newly created div to our parent div.
+    gameDisplay.appendChild(topObstacle)
 
     function moveObstacle()
     {
+        // Moving the obstacle 2 pixels to the left continuously.
         obstacleLeft -= 2
-        obstacle.style.left = obstacleLeft + 'px'
-
-        if(obstacleLeft  == -60) {
+        bottomObstacle.style.left = obstacleLeft + 'px'
+        topObstacle.style.left = obstacleLeft + 'px'
+        
+        // If the obstacle is outside our game box then we delete it.
+        if(obstacleLeft  == 0) {
             clearInterval(timerId)
-            gameDisplay.removeChild(obstacle)
+            gameDisplay.removeChild(bottomObstacle)
+            gameDisplay.removeChild(topObstacle)
         }
 
-        if(birdBottom == 0) {
+        // If the bird collides with an obstacle we called the gameOver method.
+        if(obstacleLeft > 200 && 
+            obstacleLeft < 280 && 
+            birdLeft == 220 && 
+            (birdBottom < obstacleBottom + 155 || 
+             birdBottom > obstacleTop + gap - 200) || 
+            birdBottom == 0) {
             gameOver()
         }
     }
+
+    // Timer to call the moveObstacle method every 20 ms.
     let timerId = setInterval(moveObstacle, 20)
-    setTimeout(generateObstacle, 3000)
+
+    // If the game is not over then we generate another obstacle.
+    if(!isGameOver) { 
+        setTimeout(generateObstacle, 3000)
+    }
 }
 
 generateObstacle()
 
 function gameOver()
 {
-
+    clearInterval(gameTimerId)
+    isGameOver = true;
+    document.removeEventListener('keyup', control)
 }
